4b8f57f4773476c178a2a47998dcac5105c6e566
davidsroth <davidroth6@gmail.com>
2023-05-31 12:04:52 -0400

    hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,27 @@
+function toggle_application(name)
+	local app = hs.application.get(name)
+	if not app then
+		-- The application isn't running, start it
+		hs.application.launchOrFocus(name)
+	elseif app:isFrontmost() then
+		-- The application is running and is foremost, hide it
+		app:hide()
+	else
+		-- The application is running but not foremost, focus it
+		hs.application.launchOrFocus(name)
+	end
+end
+hs.application.enableSpotlightForNameSearches(true)
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()
+
+hs.hotkey.bind({ "alt" }, "t", function()
+	toggle_application("iTerm")
+end)
+
+hs.hotkey.bind({ "alt" }, "a", function()
+	toggle_application("Arc")
+end)
+
+hs.hotkey.bind({ "alt" }, "m", function()
+	toggle_application("Messages")
+end)
