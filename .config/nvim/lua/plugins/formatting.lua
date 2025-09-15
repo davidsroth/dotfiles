f283d836030ba53e86787a533654fad96e33b39f
David Roth <davidsamuelroth@gmail.com>
2025-09-15 13:53:44 -0400

    conform format-on-save
    Signed-off-by: David Roth <davidsamuelroth@gmail.com>

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -3,30 +3,37 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
+    -- Use LazyVim's autoformat integration instead of conform's format_on_save
+    init = function()
+      -- Disable global autoformat; enable selectively below
+      vim.g.autoformat = false
+
+      local allow = {
+        python = true,
+        lua = true,
+        javascript = true,
+        typescript = true,
+        typescriptreact = true,
+        javascriptreact = true,
+        json = true,
+        toml = true,
+        yaml = true,
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("conform_selective_autoformat", { clear = true }),
+        callback = function()
+          if allow[vim.bo.filetype] then
+            vim.b.autoformat = true
+          end
+        end,
+      })
+    end,
     opts = function(_, opts)
       opts = opts or {}
-
-      -- Enable format-on-save for selected filetypes; use LSP fallback elsewhere
-      opts.format_on_save = function(bufnr)
-        local allow = {
-          python = true,
-          lua = true,
-          javascript = true,
-          typescript = true,
-          typescriptreact = true,
-          javascriptreact = true,
-          json = true,
-          toml = true,
-          yaml = true,
-        }
-        local ft = vim.bo[bufnr].filetype
-        if allow[ft] then
-          return { timeout_ms = 500, lsp_fallback = true }
-        end
-      end
-
+      -- Keep default format options used by LazyVim when formatting
+      opts.default_format_opts = { timeout_ms = 500, lsp_fallback = true }
       return opts
     end,
   },
 }
-
