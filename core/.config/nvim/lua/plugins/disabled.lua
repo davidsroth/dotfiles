21836a08b7275e341fe0f8d175b8f016310f1e7f
David Roth <davidsamuelroth@gmail.com>
2026-04-29 16:04:50 -0400

    nvim: bump lazy-lock and switch leap.nvim to codeberg mirror
    - lazy-lock.json: routine plugin updates (fzf-lua, gitsigns, mason,
      nvim-lspconfig, render-markdown, telescope, vimtex, yazi, etc.)
    - disabled.lua: point disabled leap.nvim entry at the codeberg mirror
      since the original ggandor/leap.nvim repo is gone

--- core/.config/nvim/lua/plugins/disabled.lua
+++ core/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  { "ggandor/leap.nvim", enabled = false },
+  { url = "https://codeberg.org/andyg/leap.nvim.git", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "hrsh7th/nvim-cmp", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
